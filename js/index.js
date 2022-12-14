fe6fff5753f57051a5256263f67b718358e01965
Duy. Bui Quang - CMC Global DE1 <bqduy1@cmcglobal.vn>
2022-12-14 11:34:23 +0700

    upload project code

--- js/index.js
+++ js/index.js
@@ -4,6 +4,33 @@ const rawCountryList = universityJson.map(item => item.country);
 const countryList = rawCountryList.filter((item, index, self) => self.indexOf(item) === index);
 countryList.sort();
 
-console.log(rawCountryList);
-console.log(countryList);
+// console.log(rawCountryList);
+// console.log(countryList);
 
+Vue.component("v-select", VueSelect.VueSelect);
+
+var app = new Vue ({
+
+    el: "#vue-app",
+
+    data: {
+        countries: countryList,
+        countrySelected: "",
+        universities: [],
+        universitySelected: null
+    },
+
+    methods: {
+        fetchUniversities: function() {
+
+            console.log("fetchUniversities() called for " + this.countrySelected);
+
+            let matches = universityJson.filter(item => item.country == this.countrySelected);
+            matches.sort((lhs,rhs) => lhs.name < rhs.name ? -1: 1);
+
+            console.log(matches);
+
+            this.universities = matches;
+        }
+    }
+});
